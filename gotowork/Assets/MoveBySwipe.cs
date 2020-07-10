b19ae969156f7a895a0f619ad9829d746df1ba47
rbergoglio <ramiro.bergoglio@gmail.com>
2020-07-10 04:49:28 -0300

    Joystick. player movement

---
+++ gotowork/Assets/MoveBySwipe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBySwipe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Touch touch = Input.GetTouch(0);
+        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+        touchPosition.z = 0f;
+        transform.position = touchPosition;
+    }
+}
---
+++ gotowork/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+
+    private bool jump = false;
+
+    private bool crouch = false;
+
+    public Joystick joystick;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = joystick.Horizontal * runSpeed;
+        controller.Move(horizontalMove * Time.fixedDeltaTime,jump,crouch);
+
+    }
+}
